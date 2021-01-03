d2e9fe664a10c868bd8d4c54027a687157c976f0
Mohamed Abady <mohamed.abady995@gmail.com>
2021-01-03 10:49:34 +0200

    add movie page

--- SRC/SCREENS/AddMovie.tsx
+++ SRC/SCREENS/AddMovie.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import { View, SafeAreaView, FlatList, Alert, Text, StyleSheet, StatusBar, Image } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { Colors } from '../constants';
 
 const AddMovie: React.FC = () => {
-  return <View />;
+  const [movieName, setMovieName] = useState(null);
+  const renderTextInput = (label, placeholder, onChange, value) => {
+    return (
+      <View style={{padding:12}}>
+        <Text style={{fontSize:14, color:Colors.mainColorRedDark, fontWeight:'bold', marginBottom:4}}>{label}</Text>
+        <TextInput
+          style={{borderWidth:2, borderRadius:5, borderColor:Colors.mainColorRedDark, paddingHorizontal:14}}
+          value={value}
+          placeholder={placeholder}
+          onChangeText={text => onChange(text)}
+        />
+      </View>
+    )
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={styles.imageButton}>
+        <Image source={require('../Assets/pl3.jpg')} style={styles.posterImage} />
+        <Image source={require('../Assets/white-plus-icon.png')} style={{ width: 50, height: 50, marginTop: -100, zIndex: 1000, backgroundColor: 'white' }} />
+      </TouchableOpacity>
+      {renderTextInput('Movie Name', 'enter movie name', (text)=>setMovieName(text), movieName)}
+    </SafeAreaView>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0
+  },
+  posterImage: {
+    width: '90%',
+    height: 200,
+    resizeMode: 'cover'
+  },
+  imageButton: {
+    alignItems: 'center',
+    height: 200
+  }
+})
 
 export default AddMovie;
--- SRC/SCREENS/MyMovies.tsx
+++ SRC/SCREENS/MyMovies.tsx
@@ -1,30 +1,76 @@
 import React, { useState, useEffect } from 'react';
-import { View, SafeAreaView, FlatList, Alert } from 'react-native';
+import { View, SafeAreaView, FlatList, Alert, Text, StyleSheet, StatusBar } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import MovieCard from '../Components/MovieCard';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Colors } from '../constants';
 
-const MyMovies: React.FC = () => {
-  const [myMovies, setMyMovies] = useState();
-  const storeData = async (value) => {
+const MyMovies: React.FC = ({ navigation }) => {
+  const [myMovies, setMyMovies] = useState([]);
+  const getMyMovies = async () => {
     try {
-      await AsyncStorage.setItem('MyMovies', JSON.stringify(value))
+      const jsonMoviesValue = await AsyncStorage.getItem('MyMovies');
+      jsonMoviesValue != null ? JSON.parse(jsonMoviesValue) : []
+      setMyMovies(jsonMoviesValue)
     } catch (e) {
-      Alert.alert('Error saving', e);
+      Alert.alert('Error', 'failed to get data')
     }
   }
+
+  useEffect(() => {
+    getMyMovies();
+  }, []);
+
+  const renderMovieItem = ({ item }) => {
+    return <MovieCard title={item.title} date={item.date} overview={item.overview} poster={item.poster} />
+  }
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <FlatList
-        data={moviesList}
-        renderItem={renderItem}
+        style={{ maxHeight:'80%'}}
+        data={myMovies}
+        renderItem={renderMovieItem}
         keyExtractor={item => item.id.toString()}
-        onEndReached={() => {
-          setIsEndLoading(true);
-          getMovies()
-        }}
-        ListFooterComponent={isEndLoading && <ActivityIndicator size={'large'} color={Colors.mainColorRed} style={{ height: 200 }} />}
+        ListEmptyComponent={<View style={{ justifyContent: 'center', alignItems: 'center' }}>
+          <Text style={styles.emptyText}>Your Movies List is Empty. Add some movies</Text>
+        </View>}
       />
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('AddMovie')}>
+        <Text style={styles.buttonText}>Add Movie</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0
+  },
+  emptyText: {
+    textAlign: 'center',
+    width: '80%',
+    fontSize: 32,
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  button: {
+    height: 50,
+    width: '50%',
+    backgroundColor: Colors.mainColorRedDark,
+    borderRadius: 10,
+    shadowColor: 'grey',
+    shadowOpacity: 0.7,
+    shadowOffset: { width: 0, height: 0 },
+    elevation: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 14
+  }
+})
 
 export default MyMovies;
